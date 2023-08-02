68a35d7fe01135a9c90633ab77106b464a303082
danillo <danillomello.developer@gmail.com>
2023-08-01 22:25:00 -0300

    primeira versão bot

---
+++ bot.py
@@ -0,0 +1,16 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+# pyautogui.displayMousePosition()
+
+while 1:
+    if pyautogui.locateOnScreen('stickman.png', confidence=0.3) != None:
+        print("I can see it")
+        time.sleep(0.5)
+    else :
+        print("I am unable to see it")
+        time.sleep(0.5)
---
+++ test.py
@@ -0,0 +1,3 @@
+import pyautogui
+iml = pyautogui.screenshot(region=(20,250,200,600))
+iml.save(r"C:\Danillo\Projetos-teste\python-learning\python-bot\stickman.png")
