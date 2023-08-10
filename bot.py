a13fb1d8e0a82e68c2f7b648ebe2d6d69d503e70
Danillo <danillomello87@gmail.com>
2023-08-10 18:12:40 -0300

    implementando modificações de janela

--- bot.py
+++ bot.py
@@ -1,28 +1,55 @@
 from pyautogui import *
 import pyautogui
-import time
 import keyboard
 import random
+from win32api import GetSystemMetrics
+import pygetwindow
 from pynput.keyboard import Listener, Key
 from classes import RepeatebleTimer
 
-def health(times):
-        pic = pyautogui.screenshot(region=(0,0,960,505))
-        r,g,b = pic.getpixel((213,47))
-        if g != 255:
-            keyboard.press_and_release('1')
-def spell(times):
-    keyboard.press_and_release('r')
 
-def food(times):
-    keyboard.press_and_release('4')
+def getResolution():
+    if (GetSystemMetrics(0) == 1920 & GetSystemMetrics(1) == 1080):
+        return [563,516]    
+    
+def presetsAndPrint():
+    windows = pygetwindow.getWindowsWithTitle("Mirage Realms")
+    size = getResolution()
+    y = 0
+    x = 0
+    prints = []
+    for window in windows:        
+        window.activate()    
+        window.resizeTo(size[0],size[1])
+        y = y + size[1]
+        window.moveTo(x,y)
+        prints.append(pyautogui.screenshot(region=(x,y,size[0],size[1])))       
+    return prints
+    
+def health(times, prints):
+    pic = prints[0]        
+    r,g,b = pic.getpixel((213,47))
+    if g != 255:
+        keyboard.press_and_release('1')
+def spell(times, prints):
+    pic = prints[0]
+    pixel = pic.getpixel((213,47))
+    if pixel[1] != 255:
+        keyboard.press_and_release('r')
+
+def food(times, prints):
+    pic = prints[0]
+    pixel = pic.getpixel((213,47))
+    if pixel[1] != 255:
+        keyboard.press_and_release('4')
 
 timers = []
 
 def start():
-    rt = RepeatebleTimer(120 + random.randint(0, 9), food, [0])
-    rt2 = RepeatebleTimer(60 + random.randint(0, 9), spell, [0])
-    rt3 = RepeatebleTimer(6 + random.randint(0, 9), health, [0])
+    prints = presetsAndPrint()    
+    rt = RepeatebleTimer(120 + random.randint(0, 9), food, [0, prints])
+    rt2 = RepeatebleTimer(60 + random.randint(0, 9), spell, [0, prints])
+    rt3 = RepeatebleTimer(6 + random.randint(0, 9), health, [0, prints])
     timers.extend([rt, rt2, rt3])
 
     [t.start() for t in timers]    
