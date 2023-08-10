a8efabfd7df2d575453e51d050e18296fc8ee356
danillo <danillomello.developer@gmail.com>
2023-08-10 18:39:14 -0300

    Iimplementando

--- bot.py
+++ bot.py
@@ -1,29 +1,35 @@
 from pyautogui import *
 import pyautogui
-import time
 import keyboard
 import random
 from pynput.keyboard import Listener, Key
 from classes import RepeatebleTimer
 
 def health(times):
-        pic = pyautogui.screenshot(region=(0,0,960,505))
-        r,g,b = pic.getpixel((213,47))
-        if g != 255:
-            keyboard.press_and_release('1')
-def spell(times):
-    keyboard.press_and_release('r')
-
+    pic = pyautogui.screenshot(region=(0,0,960,505))
+    pixel = pic.getpixel((213,47))
+    if pixel[1] != 255:
+        keyboard.press_and_release('1')
+def spell(times, gFixo):
+    pic = pyautogui.screenshot(region=(0,0,960,505))
+    pixel = pic.getpixel((226,452))
+    if gFixo == pixel[1]:
+        keyboard.press_and_release('r')
 def food(times):
-    keyboard.press_and_release('4')
+    pic = pyautogui.screenshot(region=(0,0,960,505))
+    pixel = pic.getpixel((473,466))
+    if 117 == pixel[1]:
+        keyboard.press_and_release('4')
 
 timers = []
 
 def start():
-    rt = RepeatebleTimer(120 + random.randint(0, 9), food, [0])
-    rt2 = RepeatebleTimer(60 + random.randint(0, 9), spell, [0])
+    rt1 = RepeatebleTimer(120 + random.randint(0, 9), food, [0])
+    pic = pyautogui.screenshot(region=(0,0,960,505))
+    pixel = pic.getpixel((226,452))
+    rt2 = RepeatebleTimer(60 + random.randint(0, 9), spell, [0, pixel[1]])
     rt3 = RepeatebleTimer(6 + random.randint(0, 9), health, [0])
-    timers.extend([rt, rt2, rt3])
+    timers.extend([rt1, rt2, rt3])
 
     [t.start() for t in timers]    
 
--- position.py
+++ position.py
@@ -2,4 +2,6 @@ import pyautogui
 
 print(pyautogui.displayMousePosition())
 # 213, 47 - 0, 255, 0
-199,465 - 166,231,228
+199,465 - 166,231,
+226,452
+473,466 - 117
