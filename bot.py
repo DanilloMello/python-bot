8f2004c872a8a86645d23917ffa3c0ed4fcfafaa
danillo <danillomello.developer@gmail.com>
2023-08-12 17:04:03 -0300

    Ajuste de reize de tela, position, threads de outras posições e recuperar pixel dinamicamente

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from re import search
+import time
 from pyautogui import *
 
 import keyboard
@@ -15,7 +16,9 @@ from classes import RepeatebleTimer
 import win32gui
 
 MSGBOX_TITLE = 'Mirage Realms'
-
+HEALTH_PIXEL = 75
+HEALTH_POSITION = [161,56] 
+SPELL_POSITION = [237,385]
 
 def callback(hwnd, custom_list):
     custom_list.append((hwnd, win32gui.GetWindowText(hwnd)))
@@ -34,29 +37,53 @@ def getWindowsSizeAndPosition():
         resizeWindow(windowsMatched[i], windowsPositionAndSize[i])
     return [windowsMatched, windowsPositionAndSize]
 
+def getSpellPixels(spp):
+    y = 0
+    pixels = []
+    for ps in spp:        
+        ss = pyautogui.screenshot(region= (ps[0],y,ps[2],ps[3]))
+        g = ss.getpixel((SPELL_POSITION[0],SPELL_POSITION[1]))
+        pixels.append(g[1])
+        y =+ ps[3]
+    return pixels
+
+def getHealthPixels(spp):
+    y = 0
+    pixels = []
+    for ps in spp:        
+        ss = pyautogui.screenshot(region= (ps[0],y,ps[2],ps[3]))
+        g = ss.getpixel((HEALTH_POSITION[0],HEALTH_POSITION[1]))
+        pixels.append(g[1])
+        y =+ ps[3]
+    return pixels
 
 def getWindowSizeByResolution():
     screenWidth = GetSystemMetrics(0)
     screenHeight = GetSystemMetrics(1)
     resolution = []
     if screenWidth == 1920 and screenHeight == 1080:
-        resolution.extend([592,509])
+        resolution.extend([583,509])
     return resolution
     
-def health(times, prints):
-    for print in prints:      
-        pixel = print.getpixel((213,47))
-        if pixel[1] != 255:            
-            keyboard.press_and_release('1')
+def health(times, wspp):
+    pixelsSource = HEALTH_PIXEL
+    for i in range(len(wspp[0])):
+        w = wspp[0][i]
+        pixelTarget = getHealthPixels(wspp[1])
+        if pixelTarget[i] == pixelsSource:
+           ActivateWindow(w)
+           keyboard.press_and_release('1')
+        time.sleep(random.randint(0,2))
             
-def spell(times, windowSP):
-    for i in range(len(windowSP[0])):    
-        ActivateWindow(windowSP[0][i])
-        pixel = pyautogui.screenshot(region= (sp[0],sp[1],sp[2],sp[3])).getpixel((219,377))
-        print(pixel)
-        if pixel[1] != 204:
-              
-            keyboard.press_and_release('r')
+def spell(times, wspp):
+    pixelsSource = getSpellPixels(wspp[1])
+    for i in range(len(wspp[0])):
+        w = wspp[0][i]
+        pixelTarget = getSpellPixels(wspp[1])
+        if pixelTarget[i] in pixelsSource:
+           ActivateWindow(w)
+           keyboard.press_and_release('r')
+        time.sleep(random.randint(0,5))
 
 def resizeWindow(window, positionAndSize):
     win32gui.MoveWindow(window, positionAndSize[0], positionAndSize[1], positionAndSize[2], positionAndSize[3], True)
@@ -77,11 +104,11 @@ def food(times, prints):
 timers = []
 
 def start():
-    windowSP = getWindowsSizeAndPosition()
-    rt = RepeatebleTimer(120 + random.randint(0, 9), food, [0, windowSP])
-    rt2 = RepeatebleTimer(60 + random.randint(0, 9), spell, [0, windowSP])
-    rt3 = RepeatebleTimer(6 + random.randint(0, 9), health, [0, windowSP])
-    timers.extend([rt2])
+    wspp = getWindowsSizeAndPosition()
+    rt = RepeatebleTimer(120 + random.randint(0, 9), food, [0, wspp])
+    rt2 = RepeatebleTimer(60 + random.randint(0, 5), spell, [0, wspp])
+    rt3 = RepeatebleTimer(6 + random.randint(0, 2), health, [0, wspp])
+    timers.extend([rt2,rt3])
 
     [t.start() for t in timers]    
 
