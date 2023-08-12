314a0f9f77344beb7d2dfea8933148e5be487905
Danillo <danillomello87@gmail.com>
2023-08-12 11:59:07 -0300

    teste

--- bot.py
+++ bot.py
@@ -4,7 +4,9 @@ from pyautogui import *
 import keyboard
 import random
 import pyautogui
-import win32gui
+import win32.lib.win32con as win32con
+import win32gui, win32com.client
+import pythoncom
 
 from win32api import GetSystemMetrics
 from pynput.keyboard import Listener, Key
@@ -14,24 +16,31 @@ import win32gui
 
 MSGBOX_TITLE = 'Mirage Realms'
 
+
 def callback(hwnd, custom_list):
     custom_list.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-def getWindows():
+def getWindowsSizeAndPosition():
     windowsMatched = []
     win32gui.EnumWindows(callback, windows := [])
     for window, title in windows:
         if search(MSGBOX_TITLE, title):
-            windowsMatched.append(window)
-    return windowsMatched
+            windowsMatched.append(window)  
+    windowSize = getWindowSizeByResolution()
+    windowsPositionAndSize = []
+    windowsPositionAndSize.extend([[0,0,windowSize[0],windowSize[1]],
+                                   [0,0 + windowSize[1],windowSize[0],windowSize[1]]])
+    for i in range(len(windowsMatched)):
+        resizeWindow(windowsMatched[i], windowsPositionAndSize[i])
+    return [windowsMatched, windowsPositionAndSize]
 
 
-def getResolution():
+def getWindowSizeByResolution():
     screenWidth = GetSystemMetrics(0)
     screenHeight = GetSystemMetrics(1)
     resolution = []
     if screenWidth == 1920 and screenHeight == 1080:
-        resolution.extend([563,516])
+        resolution.extend([592,509])
     return resolution
     
 def health(times, prints):
@@ -40,24 +49,24 @@ def health(times, prints):
         if pixel[1] != 255:            
             keyboard.press_and_release('1')
             
-def spell(times, windows):
-    resolution = getResolution()
-    widthResolution, heightResolution = 0,0
-    for window in windows:
-        resizeWindow(window, resolution)
-        win32gui.SetForegroundWindow(window)
-        ss = pyautogui.screenshot(region= (0,heightResolution,resolution[0],resolution[1]))
-        heightResolution = heightResolution + resolution[1]
-        if ss.getpixel[1] != 255:   
+def spell(times, windowSP):
+    for i in range(len(windowSP[0])):    
+        ActivateWindow(windowSP[0][i])
+        pixel = pyautogui.screenshot(region= (sp[0],sp[1],sp[2],sp[3])).getpixel((219,377))
+        print(pixel)
+        if pixel[1] != 204:
+              
             keyboard.press_and_release('r')
 
-def resizeWindow(window, resolution):
-    y =+ resolution[1]
-    x = 0
-    x0, y0, x1, y1 = win32gui.GetWindowRect(window)
-    w = x1 - x0
-    h = y1 - y0
-    win32gui.MoveWindow(window, x, y0 + y, resolution[0], resolution[1], True)
+def resizeWindow(window, positionAndSize):
+    win32gui.MoveWindow(window, positionAndSize[0], positionAndSize[1], positionAndSize[2], positionAndSize[3], True)
+    
+
+def ActivateWindow(window):
+    win32gui.ShowWindow(window, win32con.SW_NORMAL)
+    shell = win32com.client.Dispatch("WScript.Shell", pythoncom.CoInitialize())
+    shell.SendKeys('%')
+    win32gui.SetForegroundWindow(window)
 
 def food(times, prints):
     for print in prints:      
@@ -68,11 +77,10 @@ def food(times, prints):
 timers = []
 
 def start():
-    windows = getWindows()
-    print(windows)
-    rt = RepeatebleTimer(120 + random.randint(0, 9), food, [0, windows])
-    rt2 = RepeatebleTimer(5 + random.randint(0, 9), spell, [0, windows])
-    rt3 = RepeatebleTimer(6 + random.randint(0, 9), health, [0, windows])
+    windowSP = getWindowsSizeAndPosition()
+    rt = RepeatebleTimer(120 + random.randint(0, 9), food, [0, windowSP])
+    rt2 = RepeatebleTimer(60 + random.randint(0, 9), spell, [0, windowSP])
+    rt3 = RepeatebleTimer(6 + random.randint(0, 9), health, [0, windowSP])
     timers.extend([rt2])
 
     [t.start() for t in timers]    
--- position.py
+++ position.py
@@ -2,6 +2,6 @@ import pyautogui
 
 print(pyautogui.displayMousePosition())
 # 213, 47 - 0, 255, 0
-199,465 - 166,231,
-226,452
-473,466 - 117
+199,465 - 166,231,228
+
+219,377-204
