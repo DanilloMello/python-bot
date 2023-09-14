8805c5b7e7bdcde044f39efc7f4897e8ceff1a94
danillo <danillomello.developer@gmail.com>
2023-09-14 12:25:49 -0300

    Alterações e esbouço

--- Application.py
+++ Application.py
@@ -7,11 +7,15 @@ class Application(Tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         Tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.windowFrame = Tk.Frame(self.parent, highlightcolor="gray", highlightthickness=5)
+        
+        self.windowFrame = self.frame(self.parent)
         self.windowFrame.pack(padx=20, pady=20)
 
         self.buttonDone = self.button(self.windowFrame, "Done", self.windowFrame.quit)
-        self.buttonDone.pack(side=RIGHT)
+        self.buttonDone.pack(padx=10, pady=10)
+
+        self.buttonDone = self.button(self.windowFrame, "Done", self.windowFrame.quit)
+        self.buttonDone.pack(padx=10, pady=10)
         
 
         self.presetFrame = Tk.Frame(self.parent)
@@ -22,13 +26,20 @@ class Application(Tk.Frame):
         button = Tk.Button(frame)
         button["text"] = title
         button["font"] = (Const.FONTS_DEFAULT_FAMILY, Const.FONTS_DEFAULT_SIZE)
-        button["width"] = 5
+        button["width"] = 10
         button["command"] = action
         return button
+    
+    def frame(self, parent):
+        frame = Tk.Frame(parent)
+        frame['borderwidth'] = 2
+        frame['relief'] = 'solid'
+        return frame
         
 
 if __name__ == "__main__":
     root = Tk.Tk()
+    root.geometry("500x500")
     Application(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
 
